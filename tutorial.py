fafb3086e6102973af83ff761a924eebdfec434e
pontusli <pontusli@chalmers.se>
2023-10-06 10:51:58 +0800

    added an example for the mdp toolbox

---
+++ tutorial.py
@@ -0,0 +1,23 @@
+### basemap ###
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+map = Basemap()
+
+map.drawcoastlines()
+
+plt.show()
+plt.savefig('figures/test.png')
+
+### built in mdp package - mdptoolbox ###
+
+import mdptoolbox.example
+
+P, R = mdptoolbox.example.forest(S=3, r1=4, r2=2, p=0.1, is_sparse=False)
+vi = mdptoolbox.mdp.ValueIteration(P, R, 0.9)
+vi.run()
+print(vi.policy) # result is (0, 0, 0)
+
+
+
