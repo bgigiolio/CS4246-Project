ee90820fe130e026bbb4778af03f769dcc90ecbe
e0543403 <e0543403@u.nus.edu>
2023-10-15 15:14:35 +0800

    implement line drawing function
    Create plotActions function
    plots on basemap based on specified actions schema

--- lines.py
+++ lines.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+from enum import Enum
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
@@ -14,4 +16,27 @@ y = [20, -10, -30, -35, -30, -10, 20]
 map.plot(x, y, latlon=True)
 print(map.is_land(100, 20)) #land
 print(map.is_land(0, -35)) #ocean
+
+def plotActions(map: Basemap, start: tuple[float, float], actions: list[int] = [], granularity: float = 0.5):
+    print(len(actions))
+    print(granularity)
+
+    prevPoint = start
+    for i in actions:
+        if i == 0:
+            start = (start[0] + granularity, start[1])    
+        elif i == 1:
+            start = (start[0] , start[1] + granularity)
+        elif i == 2:
+            start = (start[0] - granularity, start[1])
+        elif i == 3:
+            start = (start[0], start[1] - granularity)
+        else:
+            raise NotImplementedError("invalid action argument " +  str(i))
+        
+        map.plot([prevPoint[0], start[0]], [prevPoint[1], start[1]], color="b")
+        prevPoint = start
+
+plotActions(map, (-50,-50), [1,1,0,0,0], 5)
+
 plt.show()
