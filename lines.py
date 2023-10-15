6345a9e9f98b2d5f79d78276e383f09f587d10dd
e0543403 <e0543403@u.nus.edu>
2023-10-15 15:16:30 +0800

    remove unnecessary code
    remove unnecessary code

--- lines.py
+++ lines.py
@@ -7,16 +7,6 @@ map = Basemap()
 
 map.drawcoastlines()
 
-#latitude (-90, 90)
-#longitude (-180, 180)
-
-x = [-100, -70, -30, 0, 30, 70, 100]
-y = [20, -10, -30, -35, -30, -10, 20]
-
-map.plot(x, y, latlon=True)
-print(map.is_land(100, 20)) #land
-print(map.is_land(0, -35)) #ocean
-
 def plotActions(map: Basemap, start: tuple[float, float], actions: list[int] = [], granularity: float = 0.5):
     print(len(actions))
     print(granularity)
@@ -34,7 +24,7 @@ def plotActions(map: Basemap, start: tuple[float, float], actions: list[int] = [
         else:
             raise NotImplementedError("invalid action argument " +  str(i))
         
-        map.plot([prevPoint[0], start[0]], [prevPoint[1], start[1]], color="b")
+        map.plot([prevPoint[0], start[0]], [prevPoint[1], start[1]], color="b", latlon=True)
         prevPoint = start
 
 plotActions(map, (-50,-50), [1,1,0,0,0], 5)
