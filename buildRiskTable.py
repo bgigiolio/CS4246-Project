39ac496dca592de82c24cc8ddd503bf637ddd8af
Blake Gigiolio <80730396+bgigiolio@users.noreply.github.com>
2023-10-19 22:18:30 +0800

    Interfacing with dataset to create danger heat map

--- buildRiskTable.py
+++ buildRiskTable.py
@@ -1,35 +1,47 @@
 from mpl_toolkits.basemap import Basemap
 import pandas as pd
 import math
-
-def riskCalc(lon: float, lat: float) -> float:
+import os
+from dataset import Dataset
+import tqdm
+# TODO: USE HIGHER REZ MAP
+def riskCalcNone(lon: float, lat: float, data: Dataset = None) -> float:
     return 1
+
+def riskCalcNeighbors(lon: float, lat: float, data: Dataset):
+    if (round(lon, 4), round(lat, 4)) in data.states:
+        return data.states[(round(lon, 4), round(lat, 4))]["danger"]
+    else:
+        # print((round(lon, 4), round(lat, 4))
+        return "-"
     
-def generateFrame(lat: tuple[float, float] = (-90, 90), lon: tuple[float, float] = (-180, 180), scale: float = .5, riskFunc: callable = None) -> pd.DataFrame:
+def generateFrame(lat: tuple[float, float] = (-90, 90), lon: tuple[float, float] = (-180, 180), scale: float = .5, riskFunc: callable = riskCalcNone, data: Dataset = None) -> pd.DataFrame:
     """
     Generates a dataframe of size |lat[1] - lat[0| x |lon[1] - lon[0]| * 1/scale
     Columns correspond to lattitudes
     Rows/indices correspond to longitudes
-    The values of each position is decided by riskFunc
+    The values of each position is decided by riskFunc(lon, lat)
     """
-    map = Basemap()
+    # if os.path.isfile(f"riskMaps/{lat}_{lon}_{scale}.csv"):
+    #     return loadFrame(f"riskMaps/{lat}_{lon}_{scale}.csv")
+    map = Basemap(resolution="h")
     if lat[0] < -90 or lat [1] > 90:
         print("Lattitude out of bounds")
     if lon[0] < -180 or lon[1] > 180:
         print("Longitude out of bounds")
     lats = []
     lons = []
-    for t in range(math.floor(lat[0] / scale), math.ceil(lat[1] / scale)):
+    for t in range(math.ceil(lat[0] / scale), math.floor(lat[1] / scale)):
         lats.append(t * scale)
-    for n in range(math.floor(lon[0] / scale), math.ceil(lon[1] / scale)):
+    for n in range(math.ceil(lon[0] / scale), math.floor(lon[1] / scale)):
         lons.append(n * scale)
     df = pd.DataFrame(index=lons, columns=lats)
-    for t in range(math.floor(lat[0] / scale), math.ceil(lat[1] / scale)):
-        for n in range(math.floor(lon[0] / scale), math.ceil(lon[1] / scale)):
+    for t in tqdm.tqdm(range(math.ceil(lat[0] / scale), math.floor(lat[1] / scale)), desc="test"):
+        for n in range(math.ceil(lon[0] / scale), math.floor(lon[1] / scale)):
             lattitude = t * scale
             longitude = n * scale
             if not map.is_land(longitude, lattitude):
-                df.loc[longitude, lattitude] = riskFunc(n, t)
+                df.loc[longitude, lattitude] = riskFunc(longitude, lattitude, data)
             else:
                 df.loc[longitude, lattitude] = "-"
     return df
@@ -76,10 +88,14 @@ def updateFrameByFunc(df: pd.DataFrame, lats: list[float], lons: list[float], fu
 
 
 def main():
-    lattitude = (-12.5, 19.5)
-    longitude = (91, 155)
+    lattitude = (-12.5, 31.5)
+    longitude = (88.5, 153)
     scale = .5
-    generateFrame(lattitude, longitude, scale, riskCalc).to_csv(f"riskMaps/{lattitude}_{longitude}_{scale}.csv")
+    dataset=Dataset(88.5, 153, -12.5, 31.5) #South East Asia
+    dataset.generate_states(distance=scale) #needs to be done first
+    dataset.load_pirate_data(spread_of_danger=1)
+    dataset.set_start_goal_generate_distance(start=(90, 0), goal=(150, 20))
+    generateFrame(lattitude, longitude, scale, riskCalcNeighbors, dataset).to_csv(f"riskMaps/{lattitude}_{longitude}_{scale}.csv")
 
 if __name__ == "__main__":
     main()
