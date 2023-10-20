783c6e7fc738284c5000b8fda4495814ef1f7ac2
yukesh2000 <yukesh2000@gmail.com>
2023-10-20 10:28:34 +0800

    minor changes

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from mdp_toolbox_custom import ValueIteration, PolicyIteration
 def main():
     ### CREATE DATASET ###
     #dataset=Dataset(88.6, 152.9, -12.4, 31.3) #South East Asia
+    scale = .1
+    longitude = (88.6, 90)
+    lattitude = (-12.4, -8.4)
     dataset=Dataset(88.6, 90, -12.4, -8.4)
-    dataset.generate_states(distance=1) #needs to be done first
+    dataset.generate_states(distance=scale) #needs to be done first
     dataset.load_pirate_data(spread_of_danger=1)
     dataset.set_start_goal_generate_distance(start=(90, 0), goal=(150, 20))
     print(dataset) #this shows a random example state as well as all the parameters. Note that there is no indexing of the states at this part of the project. 
@@ -19,7 +22,7 @@ def main():
     ### TRANSLATE DATASET TO MDP ###
     actions = {0: "right", 1: "up", 2: "left", 3: "down"}
     P, R = state_dict_to_P(dataset.states, actions)
-    print(P)
+    print(P, R)
 
     ### SOLVE MDP using MDP toolbox ###
     vi = PolicyIteration(P, R, 0.95)
